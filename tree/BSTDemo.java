84cc5a1df5bf74fd0453d5abd1c06754b0024e51
David Jurado <davidblanco103@gmail.com>
2018-08-17 15:09:21 -0500

    Tree implementation added

---
+++ tree/BSTDemo.java
@@ -0,0 +1,18 @@
+package com.tree;
+
+public class BSTDemo {
+
+	public static void main(String[] args) {
+		
+		BST bt= new BST();
+		
+		bt.add(bt.root, bt.NodeCreate(10));
+		bt.add(bt.root, bt.NodeCreate(12));
+		bt.add(bt.root, bt.NodeCreate(11));
+		bt.add(bt.root, bt.NodeCreate(13));
+		bt.add(bt.root, bt.NodeCreate(6));
+		bt.Search(11, bt.root);		
+	}
+	
+
+}
